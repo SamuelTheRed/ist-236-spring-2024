f8db2a2696ddf8a19ab766a5f24003e8df7b2ec2
lab1 <lab1@Conway.ITNet.local>
2024-01-17 09:29:54 -0500

    in-class small app

---
+++ week2/app-code.js
@@ -0,0 +1,51 @@
+/*
+import Image, Linking
+
+return {
+    <>
+    <StatusBar style="dark" />
+    <SafeAreaView style={styles.rootContainer}>
+        <View style={styles.imageContainer}>
+            <Image style={styles.image} source={required("./assets/images-code/download.jpg")}/>
+        </View>
+        <View style={styles.informationContainer}>
+            <Text style={styles.text}>Logan's Roadhouse</Text>
+            <Text style={styles.text} onPress={} => Linking.openurl("www.logansroadhouse.com")>www.logansroadhouse.com</Text>
+            <Text style={styles.text} onPress={} => Linking.openurl("tel:8438394666")>(843) 839-4666</Text>
+            <Text style={styles.text} onPress={} => Linking.openurl("https://maps.app.goo.gl/nPcvTSNudPN3XfJP8")>Open in Google Maps</Text>
+        </View>
+    </SafeAreaView>
+    </>
+}
+
+rootContainer {
+    flex: 1,
+    backgroundColor: "#f00",
+    alignItems: "flex-start",
+    justifyContent: "center"
+},
+imageContainer {
+    flex: 3,
+    paddingTop: 40,
+    width: "100%"
+},
+image {
+    height: 300,
+    width: "100%",
+    resizeMode: "cover",
+    borderwidth: 3,
+    borderColor: "#F30"
+}
+informationContainer {
+    flex: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000"
+},
+text {
+    fontSize: 20,
+    color: "#FEE",
+    fontStyle: "italic",
+    marginBottom: 10
+}
+*/

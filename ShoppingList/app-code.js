811fc148f7a481b59b33f830f76e3a9bd6c51714
lab1 <lab1@Conway.ITNet.local>
2024-01-17 10:27:33 -0500

    in-class progress

---
+++ ShoppingList/app-code.js
@@ -0,0 +1,74 @@
+/*
+return {
+    <>
+    <StatusBar style="light" />
+    <SafeAreaView style={styles.rootContainer}>
+
+        <View style={styles.titleContainer}>
+            <Text style={styles.title}>Shopping List</Text>
+        </View>
+
+        <View style={styles.buttonContainer}>
+            <Text style={styles.text}>Add Item Button Goes Here</Text>
+        </View>
+
+        <View style={styles.subtitleContainer}>
+            <Text style={styles.subtitle}>Items to Get</Text>
+        </View>
+
+        <View style={styles.listContainer}>
+            <Text style={styles.text}>List of Items Goes Here</Text>
+        </View>
+    </SafeAreaView>
+    </>
+}
+
+rootContainer {
+    flex: 1,
+    backgroundColor: "#1e085a",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingTop: 40
+},
+titleContainer {
+    flex: 1,
+    marginTop: 40,
+    paddingHorizontal: 30,
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderBottomRightRadius: 20,
+    borderTopLeftRadius: 20
+},
+title {
+    fontSize: 40,
+    color: "#5e08cc"
+},
+buttonCOntainer {
+    flex: 1,
+    justifyContent: "center",
+    backgroundColor: "white"
+},
+subtitleContainer {
+    flex: 1,
+    marginTop: 10,
+    paddingHorizontal: 30,
+    justifyContent: "center",
+    backgroundColor: "white",
+    borderBottomRightRadius: 20,
+    borderTopLeftRadius: 20
+},
+subtitle {
+    fontSize: 30,
+    color: "#5e08cc"
+},
+listContainer {
+    flex: 5,
+    backgroundColor: "#fff"
+},
+text {
+    fontSize: 20,
+    color: "#FEE",
+    fontStyle: "italic",
+    marginBottom: 10
+}
+*/

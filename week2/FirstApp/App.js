e6ebf40fc0cf207dbbdd3ee212301cf9db1d1ee8
SamuelTheRed <samuelshelley02@gmail.com>
2024-01-18 11:41:48 -0500

    Completed FirstApp

--- week2/FirstApp/App.js
+++ week2/FirstApp/App.js
@@ -3,18 +3,52 @@ import { StyleSheet, Text, View, Image, Linking } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar style="dark" />
+    <SafeAreaView style={styles.rootContainer}>
+        <View style={styles.imageContainer}>
+            <Image style={styles.image} source={required("./assets/images-code/download.jpg")}/>
+        </View>
+        <View style={styles.informationContainer}>
+            <Text style={styles.text}>Logan's Roadhouse</Text>
+            <Text style={styles.text} onPress={() => Linking.openURL("www.logansroadhouse.com")}>www.logansroadhouse.com</Text>
+            <Text style={styles.text} onPress={() => Linking.openURL("tel:8438394666")}>(843) 839-4666</Text>
+            <Text style={styles.text} onPress={() => Linking.openURL("https://maps.app.goo.gl/nPcvTSNudPN3XfJP8")}>Open in Google Maps</Text>
+        </View>
+    </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  rootContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f00",
+    alignItems: "flex-start",
+    justifyContent: "center"
   },
+  imageContainer: {
+    flex: 3,
+    paddingTop: 40,
+    width: "100%"
+  },
+  image: {
+    height: 300,
+    width: "100%",
+    resizeMode: "cover",
+    borderwidth: 3,
+    borderColor: "#F30"
+  },
+  informationContainer: {
+    flex: 4,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#000"
+  },
+  text: {
+    fontSize: 20,
+    color: "#FEE",
+    fontStyle: "italic",
+    marginBottom: 10
+  }
 });

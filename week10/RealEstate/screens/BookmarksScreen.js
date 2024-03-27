5e5c8b63d2750a7028ecfbd16e4e1e447a551454
Shelley <SamuelShelley@conway.itnet.local>
2024-03-27 09:06:06 -0400

    Week 10 and 11 BookmarkScreen Swap

--- week10/RealEstate/screens/BookmarksScreen.js
+++ week10/RealEstate/screens/BookmarksScreen.js
@@ -1,39 +1,10 @@
-import { View, Text, StyleSheet } from "react-native";
-import { useContext } from "react";
-import { BookmarksContext } from "../../../week11/RealEstate/store/context/bookmarks-context";
-import { LISTINGS } from "../../../week11/RealEstate/data/dummy_data";
-import List from "../../../week11/RealEstate/components/List/List";
-import Colors from "../../../week11/RealEstate/constants/colors";
+import { View, Text } from "react-native";
 
-function BookmarksScreen() {
-  const bookmarkedListingsCtx = useContext(BookmarksContext);
-  const bookmarkedListings = LISTINGS.filter((listingIem) => {
-    return bookmarkedListingsCtx.ids.includes(listingIem);
-  });
 
-  if (bookmarkedListings.length === 0) {
-    return (
-      <View style={styles.rootContainer}>
-        <Text style={styles.text}>You have no saved listings yet...</Text>
-      </View>
-    );
-  } else {
-    return <List items={bookmarkedListings} />;
-  }
+function BookmarksScreen (){
+    return <View>
+        <Text>Bookmarked Listing Screen</Text>
+    </View>
 }
 
 export default BookmarksScreen;
-
-const styles = StyleSheet.create({
-  rootContainer: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "black",
-  },
-  text: {
-    fontSize: 24,
-    fontWeight: "bold",
-    color: Colors.primary300,
-  },
-});

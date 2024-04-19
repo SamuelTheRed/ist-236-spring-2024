0c6f44ca80fd6c411c9d9526c989bd32b6d61018
SamuelTheRed <samuelshelley02@gmail.com>
2024-04-19 14:18:10 -0400

    Milestone 3 Initialization

---
+++ final/shelley_IST236_milestone3/FrequencyMusic/screens/LessonsScreen.js
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+//  Screen
+function  LessonsScreen() {
+  return <Text>This is the Music Lesson Screen</Text>;
+}
+
+export default LessonsScreen;
---
+++ final/shelley_IST236_milestone3/FrequencyMusic/screens/StoreShopScreen.js
@@ -0,0 +1,8 @@
+import { Text } from "react-native";
+
+//  Screen
+function  StoreScreen() {
+  return <Text>This is the Music Store Screen</Text>;
+}
+
+export default StoreScreen;
